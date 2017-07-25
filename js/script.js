6ac30d52424b2f3c34a29b4e8e8a4354c9ded417
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2017-07-25 13:15:38 -0700

    myapp

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function changeColorOfPage ( color ) {
+   
+    document.body.style.backgroundColor = color;
+    document.querySelector('#box').style.backgroundColor = '#fff';
+    document.querySelector('#box').style.borderColor = '#fff';
+    
+}
