8e87fc8fcbf7028f0c588073154058e7c7d51c18
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2017-07-28 02:45:24 -0700

    myapp

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-function changeColorOfPage ( color ) {
+//Javascript function called inline with onclick event handler
+/*function changeColorOfPage ( color ) {
    
     document.body.style.backgroundColor = color;
     document.querySelector('#box').style.backgroundColor = '#fff';
     document.querySelector('#box').style.borderColor = '#fff';
     
+}*/
+
+//Javascript function called with addEventListener 
+function changeColorOfPage () {
+    
+    window.document.body.style.backgroundColor = '#eee';
+    window.document.querySelector( '#box' ).style.backgroundColor = '#fff';
+    window.document.querySelector( '#box' ).style.borderColor = '#fff';
+    
 }
+
+var elem = document.querySelector( '#test' );
+elem.addEventListener( 
+    "click",
+    changeColorOfPage,
+    false
+);
+   
